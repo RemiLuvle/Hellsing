71c7d76e4d86f28c6fcf6af244ca9cd290b1e97e
RemLuvsYou <118322886+RemLuvsYou@users.noreply.github.com>
2023-01-07 13:05:52 -0500

    Add files via upload

---
+++ Misc/IUserExstenstions.cs
@@ -0,0 +1,29 @@
+﻿using HellsingCore.ButtonAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRC;
+using VRC.Core;
+using VRC.UI.Elements.Menus;
+
+namespace HellsingPc.Misc
+{
+        public static class IUserExtension
+        {
+        public static Player SelectedVRCPlayer() => APIUtils._userInterface.transform.Find("Canvas_QuickMenu(Clone)/CanvasGroup/Container/Window/QMParent/Menu_SelectedUser_Local").GetComponentInChildren<SelectedUserMenuQM>().field_Private_IUser_0.prop_String_0.ReturnUserID();
+        public static Player ReturnUserID(this string User)
+        {
+            foreach (Player player in PlayerManager.field_Private_Static_PlayerManager_0.field_Private_List_1_Player_0)
+            {
+                if (player.field_Private_APIUser_0.id == User)
+                {
+                    return player;
+                }
+            }
+            return null;
+            //fix iuser exstensions
+        }
+    }
+}
---
+++ Misc/Utils.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Linq;
+using UnityEngine;
+using VRC;
+using VRC.Core;
+using VRC.UI.Elements.Menus;
+
+namespace Utils
+{
+    class esp
+    {
+        public static bool ESP = false;
+
+
+        public static void esprefresh(VRC.Player player)
+        {
+            if (ESP == true && player != null)
+            {
+                if (player.gameObject.transform.Find("SelectRegion"))
+                {
+                    {
+                        var Renderer = player.gameObject.transform.Find("SelectRegion").GetComponent<Renderer>();
+                        HighlightsFX.field_Private_Static_HighlightsFX_0.field_Protected_Material_0.color = Color.magenta;
+                        HighlightsFX.field_Private_Static_HighlightsFX_0.Method_Public_Void_Renderer_Boolean_0(Renderer, true);
+
+                    }
+                }
+            }
+        }
+
+
+        public static void espmethod()
+        {
+            foreach (VRC.Player gameObject in PlayerManager.prop_PlayerManager_0.field_Private_List_1_Player_0.ToArray())
+            {
+                if (gameObject.transform.Find("SelectRegion"))
+                {
+                    var Renderer = gameObject.transform.Find("SelectRegion").GetComponent<Renderer>();
+                    HighlightsFX.field_Private_Static_HighlightsFX_0.field_Protected_Material_0.color = Color.magenta;
+                    HighlightsFX.field_Private_Static_HighlightsFX_0.Method_Public_Void_Renderer_Boolean_0(Renderer, ESP);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+// Ikari was here, love u ikari
