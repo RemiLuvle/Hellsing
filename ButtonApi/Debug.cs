4484d85d517bc75cfc9b75e4d2da89efdd62c0fb
RemLuvsYou <118322886+RemLuvsYou@users.noreply.github.com>
2023-01-08 21:33:16 -0500

    Add files via upload

---
+++ ButtonApi/Debug.cs
@@ -0,0 +1,33 @@
+﻿using HellsingCore.ButtonAPI;
+using MelonLoader;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
+namespace HellsingCore.ButtonAPI 
+{
+    internal class Debug
+    {
+        public static GameObject lable;
+        public static TextMeshProUGUI text;
+        public static void DebugMenu()
+        {
+            lable = UnityEngine.Object.Instantiate<GameObject>(HellsingPc.Main.UserInterface.transform.Find("Canvas_QuickMenu(Clone)/CanvasGroup/Container/Window/QMParent/Menu_Dashboard/ScrollRect/Viewport/VerticalLayoutGroup/Header_QuickLinks").gameObject, APIUtils.GetQMMenuTemplate().transform);
+            lable.transform.localPosition = new Vector3(-0.0005f, 11.2451f, 12.6858f);
+            text = lable.GetComponentInChildren<TextMeshProUGUI>();
+            text.alignment = TextAlignmentOptions.TopLeft;
+            text.color = new Color(1, 0, 0, 1);
+            text.enableAutoSizing = true;
+            lable.SetActive(true);
+            text.text = "";
+        }
+        public static void Message(string message)
+        {
+            text.text = DateTime.Now.ToString("(HH.mm.ss)") + message + "\n" + text.text;
+
+        }
+    }
+}
---
+++ Misc/LoadingMusic.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine;
+using System.IO;
+using UnityEngine.Networking;
+using System.Collections;
+
+namespace HellsingPc.Misc
+{
+   class LoadingMusic { 
+        
+        
+        static System.Random random = new System.Random();
+        static string RandomMusicFile()
+        {
+        RandomMusicFile:
+            string[] musicfiles = Directory.GetFiles(Directory.GetCurrentDirectory() + "/Mods/Hellsing/Resources/Music");
+            bool filesdeleted = false;
+            foreach (string musicfile in musicfiles)
+            {
+                if (!musicfile.EndsWith(".ogg") && !musicfile.EndsWith(".mp3"))
+                {
+                   
+                    File.Delete(musicfile);
+                    filesdeleted = true;
+                }
+            }
+            if (filesdeleted)
+            {
+                goto RandomMusicFile;
+            }
+            if (musicfiles.Length == 0)
+            {
+                throw new Exception("No Audio file Found!");
+            }
+            if (musicfiles.Length == 1)
+            {
+                return musicfiles[0];
+            }
+            int index = random.Next(musicfiles.Length);
+            return musicfiles[index - 1];
+        }
+        public static IEnumerator ChangeLoadingScreen()
+        {
+            UnityWebRequest www = UnityWebRequest.Get("file://" + RandomMusicFile());
+            www.SendWebRequest();
+            while (!www.isDone)
+            {
+                yield return null;
+            }
+            AudioClip audioClip = WebRequestWWW.InternalCreateAudioClipUsingDH(www.downloadHandler, www.url, false, false, AudioType.UNKNOWN);
+            while (!www.isDone || audioClip.loadState == AudioDataLoadState.Loading)
+            {
+                yield return null;
+            }
+            if (audioClip != null)
+            {
+                if (loadingscreenAudio != null)
+                {
+                    loadingscreenAudio.clip = audioClip;
+                    loadingscreenAudio.Play();
+                }
+            }
+        }
+        static AudioSource loadingscreenAudio;
+        public static IEnumerator LoadingScreen()
+        {
+            GameObject authentication = GameObject.Find("LoadingBackground_TealGradient_Music/LoadingSound");
+            GameObject loadingscreen = GameObject.Find("MenuContent/Popups/LoadingPopup/LoadingSound");
+            if (authentication != null)
+            {
+                authentication.GetComponent<AudioSource>().Stop();
+            }
+            if (loadingscreen != null)
+            {
+                loadingscreenAudio = loadingscreen.GetComponent<AudioSource>();
+                loadingscreenAudio.Stop();
+            }
+            UnityWebRequest www = UnityWebRequest.Get("file://" + RandomMusicFile());
+            www.SendWebRequest();
+            while (!www.isDone)
+            {
+                yield return null;
+            }
+            AudioClip audioClip = WebRequestWWW.InternalCreateAudioClipUsingDH(www.downloadHandler, www.url, false, false, AudioType.UNKNOWN);
+            while (!www.isDone || audioClip.loadState == AudioDataLoadState.Loading)
+            {
+                yield return null;
+            }
+            if (audioClip != null)
+            {
+                if (authentication != null)
+                {
+                    authentication.GetComponent<AudioSource>().clip = audioClip;
+                    authentication.GetComponent<AudioSource>().Play();
+                }
+                if (loadingscreenAudio != null)
+                {
+                    loadingscreenAudio.clip = audioClip;
+                    loadingscreenAudio.Play();
+                }
+            }
+        }
+    }
+}
